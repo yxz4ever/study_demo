120c6fb53226f99b686885cd74054c2270e0462e
莫生气 <yanxz@chinatelecom.cn>
2022-10-20 15:27:22 +0800

    spring security初始

---
+++ demo02_security/src/main/java/com/xrluo/ApplicationStart.java
@@ -0,0 +1,18 @@
+package com.xrluo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+
+@SpringBootApplication
+public class ApplicationStart
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(ApplicationStart.class, args);
+    }
+}
---
+++ demo02_security/src/main/java/com/xrluo/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.xrluo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig{
+
+
+}
