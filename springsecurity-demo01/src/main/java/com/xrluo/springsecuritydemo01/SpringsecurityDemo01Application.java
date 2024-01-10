c867b5b4da65d9a9402aa25b8e059e6f3888a51f
yxz4ever <yxz4ever@sina.com>
2024-01-10 17:05:20 +0800

    初始化springsecurity

---
+++ springsecurity-demo01/src/main/java/com/xrluo/springsecuritydemo01/SpringsecurityDemo01Application.java
@@ -0,0 +1,18 @@
+package com.xrluo.springsecuritydemo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityDemo01Application {
+
+    /**
+     * 启动器
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecurityDemo01Application.class, args);
+    }
+
+}
